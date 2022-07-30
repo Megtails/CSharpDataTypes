b09caa96bfe5771b4e400832db657c51cf783a9b
Meghan <stlmeghan@yahoo.com>
2022-07-30 13:13:29 -0500

    exercises completed

---
+++ NumericTypes/Program.cs
@@ -0,0 +1,9 @@
+﻿//Calculating area of a rectangle
+Console.WriteLine("What is the length of the rectangle in feet?");
+string strLength = Console.ReadLine();
+int length = int.Parse(strLength);
+Console.WriteLine("What is the height of the rectangle?");
+string strHeight = Console.ReadLine();
+int height = int.Parse(strHeight);
+int area = length * height;
+Console.WriteLine("The area of your rectangle is " + area);
---
+++ Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿string firstLine = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+
+Console.WriteLine(firstLine);
+Console.WriteLine("What sentence would you like to look for in the sentence above?");
+string searchTerm = Console.ReadLine();
+string comparisonSearchTerm = searchTerm.ToLower();
+string comparisonFirstLine = firstLine.ToLower();
+
+int index = comparisonFirstLine.IndexOf(comparisonSearchTerm, 0);
+int lengthSearchTerm = searchTerm.Length;
+string newLine = firstLine.Remove(index, lengthSearchTerm);
+
+if (index != -1) {
+    Console.WriteLine("true");
+    Console.WriteLine("Found at index: " + index);
+    Console.WriteLine("It is " + lengthSearchTerm + " characters long.");
+    Console.WriteLine(newLine);
+}
+else {
+    Console.WriteLine("Not found.");
+}
