69077ff41575e2aabd7320bba24420f6c29aa094
Meghan <stlmeghan@yahoo.com>
2022-07-30 13:14:41 -0500

    added more

---
+++ MoreNumericTypes/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("How many miles did you drive on your trip?");
+string strMiles = Console.ReadLine();
+int miles = int.Parse(strMiles);
+
+Console.WriteLine("How many gallons of gas did you use?");
+string strGallons = Console.ReadLine();
+int gallons = int.Parse(strGallons);
+
+int mpg = miles / gallons;
+Console.WriteLine("The MPG for the trip was: " + mpg);
